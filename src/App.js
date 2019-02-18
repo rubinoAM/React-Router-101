c281e9666f7ec2e37edb538a1606f92dc771c064
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-18 12:17:32 -0500

    Added Movies route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import MovieList from './MovieList';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
           {/* Don't use <a> tags in React Router! Use link components!*/}
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
+          <Link to="/movies">Movies</Link>
 
           {/*Use the Route component to set up a path match 
           If the path matches the browser path, it'll render whatever is given as a component prop 
@@ -23,6 +25,7 @@ class App extends Component {
           So you'll need to put exact into the Route tag to remedy that */}
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
+          <Route path="/movies" component={MovieList} />
         </div>
       </Router>
     );
