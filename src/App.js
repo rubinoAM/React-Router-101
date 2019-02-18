ea32180b58a9dc5e975cc858a4fe1b7f29d60a09
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-18 11:53:58 -0500

    Implementing routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
-//Import React Router Module
-import {BrowserRouter} from 'react-router-dom';
+//Import React Router Module - The main thing we need is BrowserRouter
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
 
 class App extends Component {
   render() {
+    //The Router holds everything it needs to control like a container
     return (
-      <div className="App">
-
-      </div>
+      <Router>
+        <div className="App">
+          {/* Use the Route component to set up a path match 
+          If the path matches the browser path, it'll render whatever is given as a component prop 
+          
+          If you have a path by itself, it'll look for that path anywhere in that URL*/}
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
     );
   }
 }
