48607a3b3ded5c55ef4a4c6da8f29b443e355a6b
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-18 12:03:28 -0500

    Added Link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 //Import React Router Module - The main thing we need is BrowserRouter
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 
@@ -12,11 +12,16 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          {/* Use the Route component to set up a path match 
+          {/* Don't use <a> tags in React Router! Use link components!*/}
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+
+          {/*Use the Route component to set up a path match 
           If the path matches the browser path, it'll render whatever is given as a component prop 
           
-          If you have a path by itself, it'll look for that path anywhere in that URL*/}
-          <Route path="/" component={Home} />
+          If you have a path by itself, it'll look for that path anywhere in that URL
+          So you'll need to put exact into the Route tag to remedy that */}
+          <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
         </div>
       </Router>
